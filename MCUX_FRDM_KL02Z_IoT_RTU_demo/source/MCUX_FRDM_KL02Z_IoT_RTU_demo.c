bb08ea846241b5b5f071b8e6a0ea7080f28fa56e
ErnestoARC <ernesto.arc@gmail.com>
2021-01-23 11:11:26 -0500

    Proyecto ejemplo para prueba de modem EC25AU

--- MCUX_FRDM_KL02Z_IoT_RTU_demo/source/MCUX_FRDM_KL02Z_IoT_RTU_demo.c
+++ MCUX_FRDM_KL02Z_IoT_RTU_demo/source/MCUX_FRDM_KL02Z_IoT_RTU_demo.c
@@ -32,6 +32,7 @@
 
 #include "sdk_mdlw_leds.h"
 #include "sdk_pph_mma8451Q.h"
+#include "sdk_pph_ec25au.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -48,17 +49,22 @@
  * Local vars
  ******************************************************************************/
 
+
 /*******************************************************************************
  * Private Source Code
  ******************************************************************************/
+void waytTime(void) {
+	uint32_t tiempo = 0xFFFFF;
+	do {
+		tiempo--;
+	} while (tiempo != 0x0000);
+}
+
+
 /*
  * @brief   Application entry point.
  */
 int main(void) {
-	status_t status;
-	uint8_t nuevo_byte_uart;
-	mma8451_data_t	mma8451_data;
-
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
@@ -68,59 +74,22 @@ int main(void) {
 
     (void)uart0Inicializar(115200);	//115200bps
     (void)i2c0MasterInit(100000);	//100kbps
+
     if (mma8451QWhoAmI() == kStatus_Success){
     	(void)mma8451QInit();	//inicializa acelerometro MMA8451Q
-    	printf("MMA8451Q detected!\r\n");
-    }else{
-    	printf("MMA8451Q error\r\n");
     }
 
 
+	if(detectarModemQuectel()==kStatus_Success){
+		encenderLedAzul();
+	}else{
+		apagarLedAzul();
+	}
+
+
     while(1) {
-    	if(uart0CuantosDatosHayEnBuffer()>0){
-    		status=uart0LeerByteDesdeBuffer(&nuevo_byte_uart);
-    		if(status==kStatus_Success){
-    			printf("\r\nkey:%c\r\n",nuevo_byte_uart);
-    			switch (nuevo_byte_uart) {
-				case 'a':
-				case 'A':
-					toggleLedAzul();
-					break;
-
-				case 'v':
-					apagarLedVerde();
-					break;
-				case 'V':
-					encenderLedVerde();
-					break;
-
-				case 'r':
-					apagarLedRojo();
-					break;
-
-				case 'R':
-					encenderLedRojo();
-					break;
-
-				case 'X':
-					if(mma8451QReadAccel(&mma8451_data)== kStatus_Success){
-						printf("x:0x%04X\r\n",mma8451_data.x_value);
-						printf("y:0x%04X\r\n",mma8451_data.y_value);
-						printf("z:0x%04X\r\n",mma8451_data.z_value);
-					}else{
-						printf("MMA8451Q error\r\n");
-					}
-					break;
-
-				case 'P':
-					i2c0MasterReadByte(&nuevo_byte_uart, 1, 0x1D, 0x09);
-					printf("REG_F_SETUP:%02X\r\n",nuevo_byte_uart);
-					break;
-				}
-    		}else{
-    			printf("error\r\n");
-    		}
-    	}
+    	waytTime();
+    	toggleLedVerde();
     }
     return 0 ;
 }
