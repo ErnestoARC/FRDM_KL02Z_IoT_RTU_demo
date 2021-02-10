04a53b868ada6cdc0bf422ba953bc02df5b6f6bd
ErnestoARC <ernesto.arc@gmail.com>
2021-02-10 00:59:14 -0500

    Crea libreria para utilizar ADC0 del microcontrolador. Por el momento tiene canal ADC fijo

---
+++ MCUX_FRDM_KL02Z_IoT_RTU_demo/sdk_hal/sdk_hal_adc.c
@@ -0,0 +1,81 @@
+/*! @file : sdk_hal_adc.c
+ * @author  Ernesto Andres Rincon Cruz
+ * @version 1.0.0
+ * @date    10/02/2021
+ * @brief   Driver para ADC
+ * @details
+ *
+*/
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "sdk_hal_adc.h"
+#include "fsl_adc16.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define DEMO_ADC16_BASE 			ADC0
+#define DEMO_ADC16_CHANNEL_GROUP 	0U
+#define DEMO_ADC16_USER_CHANNEL  	11U /* ADC0_SE11 */
+
+/*******************************************************************************
+ * Private Prototypes
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Local vars
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * Private Source Code
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Public Source Code
+ ******************************************************************************/
+status_t adcInit(void){
+	adc16_config_t adc16ConfigStruct;
+	    /*
+	     * adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceVref;
+	     * adc16ConfigStruct.clockSource = kADC16_ClockSourceAsynchronousClock;
+	     * adc16ConfigStruct.enableAsynchronousClock = true;
+	     * adc16ConfigStruct.clockDivider = kADC16_ClockDivider8;
+	     * adc16ConfigStruct.resolution = kADC16_ResolutionSE12Bit;
+	     * adc16ConfigStruct.longSampleMode = kADC16_LongSampleDisabled;
+	     * adc16ConfigStruct.enableHighSpeed = false;
+	     * adc16ConfigStruct.enableLowPower = false;
+	     * adc16ConfigStruct.enableContinuousConversion = false;
+	     */
+	    ADC16_GetDefaultConfig(&adc16ConfigStruct);	//Obtiene configuracion por defecto
+	    ADC16_Init(ADC0, &adc16ConfigStruct);	//inicializa ADC con la configuración previa
+	    ADC16_EnableHardwareTrigger(ADC0, false); //configura tipo de inicio conversión "manual"
+
+	    return(kStatus_Success);
+ }
+
+status_t adcIniciarLectura(uint32_t *resultado_adc) {
+	adc16_channel_config_t adc16ChannelConfigStruct;
+
+	adc16ChannelConfigStruct.channelNumber = DEMO_ADC16_USER_CHANNEL; //Selecciona canal ADC a utilizar
+	adc16ChannelConfigStruct.enableInterruptOnConversionCompleted = false;//Deshabilita IRQ por ADC
+	ADC16_SetChannelConfig(ADC0, DEMO_ADC16_CHANNEL_GROUP,
+			&adc16ChannelConfigStruct);	//establece canal e inicia lectura ADC
+
+	//Espera a terminar proceso de conversor ADC
+	while (0U	== (kADC16_ChannelConversionDoneFlag & ADC16_GetChannelStatusFlags(DEMO_ADC16_BASE,	DEMO_ADC16_CHANNEL_GROUP))) {
+	}
+
+	*resultado_adc = ADC16_GetChannelConversionValue(DEMO_ADC16_BASE, DEMO_ADC16_CHANNEL_GROUP);
+
+	return (kStatus_Success);
+}
