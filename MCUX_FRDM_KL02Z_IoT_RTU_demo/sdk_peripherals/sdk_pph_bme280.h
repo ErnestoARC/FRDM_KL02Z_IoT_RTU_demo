9bf2ae1e2273222378269711d4ef45ea0c03d97a
ErnestoARC <ernesto.arc@gmail.com>
2021-02-13 12:32:27 -0500

    Cambios menores comentariosy nombres de funciones

--- MCUX_FRDM_KL02Z_IoT_RTU_demo/sdk_peripherals/sdk_pph_bme280.h
+++ MCUX_FRDM_KL02Z_IoT_RTU_demo/sdk_peripherals/sdk_pph_bme280.h
@@ -24,28 +24,27 @@
 /*******************************************************************************
  * Public Definitions
  ******************************************************************************/
-#define BME280_ADDRESS  (0x76) //!< Default BME280 I2C address, if SDO is GND, its 0x76, if SDO is VDD, its 0x77,
+#define BME280_ADDRESS  (0x76) //!< BME280 Dirección I2C.
 
-
+/*! @brief Listado de registros del sensor BME280 */
 enum _bme280_registers{
-	BME850_REG_ID=0xD0,
-	BME850_REG_CTRL_HUM=0XF2,
-	BME850_REG_STATUS,
-	BME850_REG_CTRL_MEAS,
-	BME850_REG_CONFIG,
-	BME850_REG_PRESS_MSB=0XF7,
-	BME850_REG_PRESS_lSB,
-	BME850_REG_PRESS_XlSB,
-	BME850_REG_TEMP_MSB,
-	BME850_REG_TEMP_lSB,
-	BME850_REG_TEMP_XlSB,
-	BME850_REG_HUM_MSB,
-	BME850_REG_HUM_lSB,
+	BME280_REG_ID=0xD0,
+	BME280_REG_CTRL_HUM=0XF2,
+	BME280_REG_STATUS,
+	BME280_REG_CTRL_MEAS,
+	BME280_REG_CONFIG,
+	BME280_REG_PRESS_MSB=0XF7,
+	BME280_REG_PRESS_lSB,
+	BME280_REG_PRESS_XlSB,
+	BME280_REG_TEMP_MSB,
+	BME280_REG_TEMP_lSB,
+	BME280_REG_TEMP_XlSB,
+	BME280_REG_HUM_MSB,
+	BME280_REG_HUM_lSB,
 };
 
 /*!
- * @brief The MMA8451 three-axis accelerometer data structure
- *
+ * @brief Define tipo de datos estructura que permite organizar los datos del sensor BM280
  */
 typedef struct _bme280_data{
 	uint32_t temperatura;
@@ -64,9 +63,9 @@ typedef struct _bme280_data{
  * Public Prototypes
  ******************************************************************************/
 /*!
- * @brief read Who Am I register for BME280 detection
+ * @brief Lee registro de identificacion sensor BME280
  *
- * @return	execution error code
+ * @return	resultado ejecución
  * @code
  * 		kStatus_Success
  * 		kStatus_Fail
@@ -75,9 +74,9 @@ typedef struct _bme280_data{
 status_t bme280WhoAmI(void);
 
 /*!
- * @brief read temperature, humidity and pressure from BME280
- * @param data	data structure pointer
- * @return	execution error code
+ * @brief Lee temperatura, humedad y presión del sensor BME280
+ * @param data	apuntador de memoria  a estructura donde almacenar los  datos
+ * @return	resultado ejecución
  * @code
  * 		kStatus_Success
  * 		kStatus_Fail
@@ -86,8 +85,8 @@ status_t bme280WhoAmI(void);
 status_t	bme280ReadData(bme280_data_t *data );
 
 /*!
- * @brief perform initialization of BME280
- * @return	execution error code
+ * @brief Inicialización de registros en sensor BME280
+ * @return	resultado ejecución
  * @code
  * 		kStatus_Success
  * 		kStatus_Fail
